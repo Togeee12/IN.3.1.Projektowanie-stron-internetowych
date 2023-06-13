568e24d81fbf9e4bc7797afbb2922567c306ab17
Togeee12 <stach.szymczykowski@gmail.com>
2023-06-13 13:13:46 +0200

    Minor fixes in kontakt, some bigger ones in main

--- Mini Kurs HTML/assets/scripts/index.js
+++ Mini Kurs HTML/assets/scripts/index.js
@@ -1,3 +1,17 @@
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
+
 const ANIMATION_DURATION = 300;
 
 const SIDEBAR_EL = document.getElementById("sidebar");
